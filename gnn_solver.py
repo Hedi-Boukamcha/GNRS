32bcc3390719be956160183d04bdf7d839af8e2b
Anas Neumann <anas.neumann.isamm@gmail.com>
2025-08-05 17:08:39 +0100

    Test for a better Gantt chart (with P and S)

--- gnn_solver.py
+++ gnn_solver.py
@@ -67,20 +67,20 @@ class Environment:
         self.ub_cmax            = ub_cmax
         self.ub_delay           = ub_delay
 
-def search_possible_decisions(state: State, possible_parallel: bool, needed_parallel: bool, device: str, env: Environment) -> list[Decision]:
+def search_possible_decisions(state: State, possible_parallel: bool, needed_parallel: bool, device: str, env: Environment, comp: int=None) -> list[Decision]:
     M1_decisions: list[Decision] = []
     M2_decisions: list[Decision] = []
     for j in state.job_states:
         for o in j.operation_states:
             if o.remaining_time > 0:
                 if o.operation.type == MACHINE_1:
-                    M1_decisions.append(Decision(job_id=j.id, job_id_in_graph=j.graph_id, operation_id=o.id, machine=o.operation.type, parallel=True))
-                    M1_decisions.append(Decision(job_id=j.id, job_id_in_graph=j.graph_id, operation_id=o.id, machine=o.operation.type, parallel=False))
+                    M1_decisions.append(Decision(job_id=j.id, job_id_in_graph=j.graph_id, operation_id=o.id, machine=o.operation.type, parallel=True, comp=-1))
+                    M1_decisions.append(Decision(job_id=j.id, job_id_in_graph=j.graph_id, operation_id=o.id, machine=o.operation.type, parallel=False, comp=-1))
                 else:
                     if possible_parallel:
-                        M2_decisions.append(Decision(job_id=j.id, job_id_in_graph=j.graph_id, operation_id=o.id, machine=o.operation.type, parallel=True))
+                        M2_decisions.append(Decision(job_id=j.id, job_id_in_graph=j.graph_id, operation_id=o.id, machine=o.operation.type, parallel=True, comp=comp))
                     if not needed_parallel :
-                        M2_decisions.append(Decision(job_id=j.id, job_id_in_graph=j.graph_id, operation_id=o.id, machine=o.operation.type, parallel=False))
+                        M2_decisions.append(Decision(job_id=j.id, job_id_in_graph=j.graph_id, operation_id=o.id, machine=o.operation.type, parallel=False, comp=-1))
                 break
     decisions: list[Decision] = M2_decisions + (M1_decisions if (not needed_parallel or len(M2_decisions)==0) else [])
     decisionsT: Tensor = torch.tensor([[d.job_id_in_graph, d.machine, float(d.parallel), env.init_UB_cmax, env.init_UB_delay, env.ub_cmax, env.ub_delay, env.cmax, env.delay, env.total_jobs, env.rm_jobs, env.m2_parallel, env.action_time] for d in decisions], dtype=torch.float32, device=device)
@@ -107,7 +107,7 @@ def solve_one(agent: Agent, gantt_path: str, path: str, size: str, id: str, impr
         state.compute_obj_values_and_upper_bounds(unloading_time=0, current_time=0)
         graph: HeteroData    = state.to_hyper_graph(last_job_in_pos=last_job_in_pos, current_time=0, device=device)
         env: Environment     = Environment(graph=graph, possible_decisions=None, decisionsT=None, init_UB_cmax=state.ub_cmax, init_UB_delay=state.ub_delay, n=len(i.jobs))
-        env.possible_decisions, env.decisionsT = search_possible_decisions(state=state, possible_parallel=(last_job_in_pos>=0), needed_parallel=next_M2_parallel, env=env, device=device)
+        env.possible_decisions, env.decisionsT = search_possible_decisions(state=state, possible_parallel=(last_job_in_pos>=0), needed_parallel=next_M2_parallel, env=env, comp=-1, device=device)
         while env.possible_decisions:
             action_id: int = agent.select_next_decision(graph=env.graph, possible_decisions=env.possible_decisions, decisionsT=env.decisionsT, eps_threshold=eps_threshold, train=train, greedy=g)
             d: Decision = env.possible_decisions[action_id]
@@ -126,7 +126,7 @@ def solve_one(agent: Agent, gantt_path: str, path: str, size: str, id: str, impr
                     m2 += 1
             state = simulate(state, d=d, clone=False)
             next_graph: HeteroData = state.to_hyper_graph(last_job_in_pos=last_job_in_pos, current_time=env.action_time, device=device)
-            next_possible_decisions, next_decisionT = search_possible_decisions(state=state, possible_parallel=(last_job_in_pos>=0), needed_parallel=next_M2_parallel, env=env, device=device)
+            next_possible_decisions, next_decisionT = search_possible_decisions(state=state, possible_parallel=(last_job_in_pos>=0), needed_parallel=next_M2_parallel, env=env, comp=last_job_in_pos, device=device)
             if train:
                 final: bool   = len(next_possible_decisions) == 0
                 _r: Tensor    = reward(env=env, cmax_new=state.start_time, delay_new=state.total_delay, ub_cmax_new=state.ub_cmax, ub_delay_new=state.ub_delay, device=device)
--- heuristic_solver.py
+++ heuristic_solver.py
@@ -39,7 +39,7 @@ def solve_one(path: str, gantt_path: str, size: str, id: str):
     for index in sorted_jobs:
         j: Job = index.job
         for o_id, o in enumerate(j.operations):
-            decisions.append(Decision(job_id=index.idx, operation_id=o_id, job_id_in_graph=0, machine=o.type, parallel=False))
+            decisions.append(Decision(job_id=index.idx, operation_id=o_id, job_id_in_graph=0, machine=o.type, parallel=False, comp=-1))
 
     # Stage 2. Improve the solution using our local search!
     state: State = LS(i, decisions)
